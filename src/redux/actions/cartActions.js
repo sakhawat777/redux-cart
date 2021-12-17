29f7399e71969f38d042fe68df9b090739dd16cc
sakhawat777 <biplobbms@gmail.com>
2021-12-17 21:15:16 +0600

    Actions and Reducers Done

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -1,2 +1,11 @@
-const ADD_TO_CART = 'ADD_TO_CART';
-const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+// Declare Actions Constance
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+
+// Actions Method
+export const addToCart = (id) => {
+	return { type: ADD_TO_CART, id };
+};
+export const removeFromCart = (id) => {
+	return { type: REMOVE_FROM_CART, id };
+};
